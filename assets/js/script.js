7418b644cbf94ab0c8714775053bcc44456efd80
Hsiaohan-89 <>
2023-11-01 11:14:37 +0000

    Add javescript array for questions

---
+++ assets/js/script.js
@@ -0,0 +1,83 @@
+const question = [
+    {
+        question: = "/asstes/images/Austrila.jpg"
+        answers: [
+            { text: "Germany", correct: false },
+            { text: "Australia", correct: true },
+            { text: "New Zealand", correct: false },
+        ]
+    },
+    {
+        question: = "/asstes/images/Brazil.jpg"
+        answers: [
+            { text: "United States", correct: false },
+            { text: "Mexico", correct: false },
+            { text: "Brazil", correct: true },
+        ]
+    },
+    {
+        question: = "/asstes/images/Egypt.jpg"
+        answers: [
+            { text: "Sudan", correct: false },
+            { text: "Egypt", correct: true },
+            { text: "Jordan", correct: false },
+        ]
+    },
+    {
+        question: = "/asstes/images/Japan.jpg"
+        answers: [
+            { text: "Japan", correct: true },
+            { text: "France", correct: false },
+            { text: "Korea", correct: false },
+        ]
+    },
+    {
+        question: = "/asstes/images/UK.jpg"
+        answers: [
+            { text: "Germany", correct: false },
+            { text: "Ireland", correct: false },
+            { text: "United Kimdom", correct: true },
+        ]
+    },
+    {
+        question: = "/asstes/images/Italy.jpg"
+        answers: [
+            { text: "Italy", correct: true },
+            { text: "Portugal", correct: false },
+            { text: "Slovenia", correct: false },
+        ]
+    },
+    {
+        question: = "/asstes/images/Turkey.jpg"
+        answers: [
+            { text: "Plovdiv", correct: false },
+            { text: "Serbia", correct: false },
+            { text: "Turkey", correct: true },
+        ]
+    },
+    {
+        question: = "/asstes/images/Korea.jpg"
+        answers: [
+            { text: "Korea", correct: true },
+            { text: "China", correct: false },
+            { text: "Taiwan", correct: false },
+        ]
+    },
+    {
+        question: = "/asstes/images/Malasia.jpg"
+        answers: [
+            { text: "Indonesia", correct: false },
+            { text: "Malaysia", correct: true },
+            { text: "Thailand", correct: false },
+        ]
+    },
+    {
+        question: = "/asstes/images/Singapore.jpg"
+        answers: [
+            { text: "Thanland", correct: false },
+            { text: "Taiwan", correct: false },
+            { text: "Singapore", correct: true },
+        ]
+    },
+];
+
